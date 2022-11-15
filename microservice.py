35746a4726bcd9eeb7c7363f6c5832e4d59eb168
StjepanR <sr51641@fer.hr>
2022-11-15 23:53:07 +0100

    remove logging

--- microservice.py
+++ microservice.py
@@ -1,14 +1,10 @@
-import logging
-
 from flask import Flask, abort
 from integration import compute, compute_all
 
 app = Flask(__name__)
-logging.basicConfig(filename='/home/stjepan/record.log', level=logging.DEBUG)
 
 @app.route('/<lower>/<upper>')
 def get_integral(lower, upper):
-    app.logger.debug("-- in controller --")
 
     try:
         lower = float(lower)
@@ -19,5 +15,4 @@ def get_integral(lower, upper):
     return ", ".join([str(x) for x in compute_all(lower, upper)])
 
 if __name__=="__main__":
-    app.logger.debug("-- starting app --")
     app.run(host='0.0.0.0')
